e6aebef18c51bdd859cb7f4f6975d11950eee481
KingOfKings01 <aasifkhan9605@gmail.com>
2024-10-26 17:42:41 +0530

    Login page and functions are added on both cline and server.

--- mail_box_server/routes/userRoutes.js
+++ mail_box_server/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createUser } from "../controllers/userControllers.js";
+import { createUser, loginUser } from "../controllers/userControllers.js";
 
 const router = Router();
 
 router.get('/test', (req, res) => {
     res.json({ message: "This is the sign-in endpoint" });
 })
+
 router.post("/sing-in", createUser);
+router.post("/login", loginUser)
 
 export default router
